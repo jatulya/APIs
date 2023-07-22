260c00323d1a4414fd3d77c2187fc52b1f424320
J-Atulya <atulyajayakrishnan@gmail.com>
2023-07-22 18:37:03 +0530

    Changed the folders

--- runtime.js
+++ runtime.js
@@ -1,14 +1,12 @@
 const toAdd = document.getElementById("addBooks")
 const addForm = document.getElementsByClassName("add-books")[0]
-console.log(addForm)
 const toRem = document.getElementById("remBooks")
 const remForm = document.getElementsByClassName("rem-books")[0]
-console.log(remForm)
 
+/* to display and remove the form when the button is clicked */
 toAdd.addEventListener("click",() => update(addForm))
 toRem.addEventListener('click',() => update(remForm))
 
 function update(idname){
-    console.log(idname)
-    idname.classList.add('show')
+    idname.classList.add('show') //shows the form
 }

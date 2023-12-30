223e39e224a8934b1addf3460dec75a7fbffd89e
J-Atulya <atulyajayakrishnan@gmail.com>
2023-12-31 01:16:03 +0530

    made a react app- started

---
+++ Books/googlebooks/src/Components/Main.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Main= () =>  {
+    return (
+      <>
+        <div className='header'>
+            <div className='row1'>
+                <h1>A room without books is like <br/>
+                a body without a soul.</h1>
+            </div>
+            <div className='row2'>
+                <h2>Find your Book</h2>
+                <div className='search'>
+                    <input type="text" placeholder="Enter the book name.."/>
+                    <button><i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+            </div>
+        </div>
+      </>
+    )
+}
+
+export default Main
+
+

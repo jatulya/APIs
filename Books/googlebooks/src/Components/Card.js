8c3db4f9a8fc4fb3af27db53e822f45d7aa0ce75
J-Atulya <atulyajayakrishnan@gmail.com>
2023-12-31 11:50:17 +0530

    Search val is stored

---
+++ Books/googlebooks/src/Components/Card.js
@@ -0,0 +1,17 @@
+import react from "react"
+
+const Card=()=>{
+    return (
+        <>
+            <div className="card">
+               <img src="./images/pjo.webp" alt="The Battle of the Labyrynth" />
+               <div className="bottom">
+                    <h3 className="title">The Battle of The Labyrinth</h3>
+                    <p className="amount">&#8377;480</p>
+                </div> 
+            </div>
+        </>
+    )
+}
+
+export default Card
--- Books/googlebooks/src/Components/Main.js
+++ Books/googlebooks/src/Components/Main.js
@@ -1,21 +1,46 @@
-import React from 'react'
+import react, { useState } from 'react'
+import Card from './Card'
+import axios from 'axios'
 
 const Main= () =>  {
+    const [search, setSearch] = useState("")
+    
+    const searchBook = () => {
+        console.log(`Value of the book is: ${search}`)
+    }
+
     return (
       <>
         <div className='header'>
+            
             <div className='row1'>
                 <h1>A room without books is like <br/>
                 a body without a soul.</h1>
             </div>
+            
             <div className='row2'>
                 <h2>Find your Book</h2>
+                
                 <div className='search'>
-                    <input type="text" placeholder="Enter the book name.."/>
+                    <input type="text" placeholder="Enter the book name.."
+                    value={search} onChange={(e) => setSearch(e.target.value)} 
+                    onKeyPress = {(e) => {
+                        e.key === "Enter" && searchBook()
+                      }}/>
                     <button><i class="fa-solid fa-magnifying-glass"></i></button>
                 </div>
+                
                 <img src='.\images\bg2.png' alt='Children reading books' />
             </div>
+        </div>
+
+        <div className='container'>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
         </div>
       </>
     )

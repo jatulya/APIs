ed73b7279cd1e235f1ceeaa427ac632182a0c724
J-Atulya <atulyajayakrishnan@gmail.com>
2024-01-02 13:59:20 +0530

    Displayed content accordingly

--- Books/googlebooks/src/Components/Card.js
+++ Books/googlebooks/src/Components/Card.js
@@ -6,19 +6,43 @@ const Card=({book})=>{
     return (
         <>
             {   //map is used to iterate over the array
+                
                 book.map((item) => {
-                    
-                    return (
-                        <>
-                        <div className="card">
-                        <img src="./images/pjo.webp" alt="The Battle of the Labyrynth" />
-                        <div className="bottom">
-                                <h3 className="title">The Battle of The Labyrinth</h3>
-                                <p className="amount">&#8377;480</p>
-                            </div> 
-                        </div>
-                        </>
-                    )
+
+                    let title = item.volumeInfo.title
+                    let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail
+                    let amount 
+                    console.log(title)
+                    if (item.saleInfo.saleability === "FOR_SALE")
+                    {
+                        amount =  item.saleInfo.listPrice.amount //undefined when item is not for sale
+                    }
+                    else if (item.saleInfo.saleability === "FREE")
+                    {
+                        amount = 0
+                    }
+
+                    //we got the names of the keys when we displayed items in the console
+                    if(thumbnail !== undefined && amount!==undefined)
+                    {
+                        return (
+                            <>
+                            <div className="card">
+                            <img src={thumbnail} alt="The Battle of the Labyrynth" />
+                            <div className="bottom">
+                                    
+                                    <h3 className="title">{title}</h3>
+                                   
+                                    { amount === 0 ?
+                                      (<p className="amount">FREE</p>) 
+                                      :
+                                      (<p className="amount">&#8377;{amount}</p>) 
+                                    }
+                                </div> 
+                            </div>
+                            </>
+                        )
+                    } 
                 })
             }
         </>

bcb0af07a5399256a469997b6e8b459fd5ac87ba
J ATULYA <109218299+jatulya@users.noreply.github.com>
2025-08-27 23:43:55 +0530

    Feat/anime search/sidebar UI (#5) (#6)
    * refactor: different sizes for headings (#4)
    
    * refactor: different sizes for headings
    
    * refactor: common css properties made into variiable
    
    * feat: sidebar ui
    
    * feat: sidebar title alignment fix

--- Anime/animesearch/src/ui/Header.tsx
+++ Anime/animesearch/src/ui/Header.tsx
@@ -4,15 +4,21 @@ import type { HeaderProps } from "../interfaces/types/propTypes";
 export const Header: React.FC<HeaderProps> = ({
   title,
   variant = "primary",
+  claassName,
 }) => {
   const headerClass = `m-4 text-center text-dark`;
-  return variant === "primary" ? (
-    <h1 className={`box text-5xl font-bold ${headerClass}`}>
+
+  let variantClass = "";
+  if (variant === "primary") 
+      variantClass = "box text-5xl font-bold";
+  else if( variant === "secondary") 
+      variantClass = "box-sm text-3xl font-semibold"
+  else
+      variantClass = "text-2xl font-semibold";
+
+    return (
+    <h1 className={`${headerClass} ${variantClass} ${claassName}`}>
       {title}
     </h1>
-  ) : (
-    <h2 className={`box-sm text-3xl font-semibold ${headerClass}`}>
-      {title}
-    </h2>
-  );
+  ); 
 };
